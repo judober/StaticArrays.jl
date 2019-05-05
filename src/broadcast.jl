ee83fa5dc572ddab4a40924ae2581bfc78106ab3
judober <16521254+judober@users.noreply.github.com>
2019-05-05 14:49:21 +0200

    Revert "update counter in broadcast generated function to handle zero dimensional case"
    This reverts commit ac97e13d67c96739ca93e4df641cadecddd1b190.

--- src/broadcast.jl
+++ src/broadcast.jl
@@ -164,23 +164,19 @@ end
         exprs[current_ind...] = :(dest[$j] = f($(exprs_vals...)))
 
         # increment current_ind (maybe use CartesianIndices?)
-	if length(current_ind) >= 1
-            current_ind[1] += 1
-            for i ∈ 1:length(newsize)
-                if current_ind[i] > newsize[i]
-                    if i == length(newsize)
-                        more = false
-                        break
-                    else
-                        current_ind[i] = 1
-                        current_ind[i+1] += 1
-                    end
-                else
+        current_ind[1] += 1
+        for i ∈ 1:length(newsize)
+            if current_ind[i] > newsize[i]
+                if i == length(newsize)
+                    more = false
                     break
+                else
+                    current_ind[i] = 1
+                    current_ind[i+1] += 1
                 end
+            else
+                break
             end
-        else
-            break
         end
         j += 1
     end
